0bb493777b88210199411e6248de05b622ff47a0
oleeriksen <oe@easv.dk>
2019-11-11 16:57:16 +0100

    MSTest added

---
+++ MSTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD01;
+
+namespace MSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+   
+        public void TestClear()
+        {
+            ICalculator c = new CalculatorImpl();
+            Assert.AreEqual(0, c.Current);
+            c.Add(4);
+            c.Clear();
+            Assert.AreEqual(0, c.Current);
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            ICalculator c = new CalculatorImpl();
+            Assert.AreEqual(0, c.Current);
+            c.Add(4);
+            Assert.AreEqual(4, c.Current);
+        }
+
+    }
+}
