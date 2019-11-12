806aa7ee682eaeca3adcea173ee8372251d5ebf5
oleeriksen <oe@easv.dk>
2019-11-12 10:32:35 +0100

    runtime fejl

--- TDD01Tests/CalculatorTest.cs
+++ TDD01Tests/CalculatorTest.cs
@@ -30,7 +30,7 @@ namespace TDD01Tests
         public void TestAdd2()
         {
             ICalculator c = new CalculatorImpl();
-            Assert.Equal(0, c.Current);
+            Assert.Equal(1, c.Current);
             c.Add(-4);
             Assert.Equal(-4, c.Current);
         }
